adf97dde84b1e4276c822ed8d3d32283c76c819b
il-stakeda <80619381+il-stakeda@users.noreply.github.com>
2025-01-02 19:48:37 +0900

    DXC20 shop files
    for DXC20 learning

---
+++ shop/common.php
@@ -0,0 +1,17 @@
+<?php
+  session_start();
+  function connect() {
+
+$user = "admin";
+$password = "PasswordMySQL";
+$dns="database-takedasaori.ch18xqw1pgc8.ap-northeast-1.rds.amazonaws.com";
+
+    return new PDO("mysql:host=$dns; dbname=shop; charset=utf8", "$user", "$password");
+  }
+
+  function img_tag($code) {
+    if (file_exists("images/$code.jpg")) $name = $code;
+    else $name = 'noimage';
+    return '<img src="images/' . $name . '.jpg" alt="">';
+  }
+?>
