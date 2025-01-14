2b6f1794f9548724b3c1ee63c2a76b8204f3129a
il-stakeda <80619381+il-stakeda@users.noreply.github.com>
2025-01-14 16:51:37 +0900

    Update killjobs.sh

--- killjobs.sh
+++ killjobs.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# 初期カウンタを 1 に設定
-i=1
+# ps コマンドで実行中のプロセスIDを取得
+P=$(ps aux | grep '[y]es' | awk '{print $2}')
 
-# カウンタが 50 以下の場合にループを実行
-while [ $i -le 50 ]; do
-    # 'yes > /dev/null' をバックグラウンドで実行
-    yes > /dev/null &
-    
-    # カウンタを 1 増加
-    ((i++))
-done
+# 環境変数に設定
+export P
 
-# バックグラウンドで実行中のプロセスの確認
-echo "50 processes have been started in the background."
+# 環境変数を表示
+kill -9 $P
+echo "50 processes killed."
